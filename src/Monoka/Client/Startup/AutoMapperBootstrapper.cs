65a606fe6a2cf4ac33a449b1795ed3567cba6056
Eirik Årdal <eaardal@outlook.com>
2016-11-19 10:58:34 +0100

    Added automapper mappings

--- src/Monoka/Client/Startup/AutoMapperBootstrapper.cs
+++ src/Monoka/Client/Startup/AutoMapperBootstrapper.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using Autofac;
 using AutoMapper;
+using Monoka.Client.GameLobby.Results;
+using Monoka.Client.Model;
+using Monoka.Common.Dto;
+using Monoka.Common.Network.Messages;
 
 namespace Monoka.Client.Startup
 {
@@ -28,9 +32,30 @@ namespace Monoka.Client.Startup
             builder.Update(container);
         }
 
-        private static void Configure(IMapperConfigurationExpression map)
+        private static void Configure(IProfileExpression config)
         {
-            
+            MapNetworkMessages(config);
+            MapDtos(config);
+        }
+
+        private static void MapNetworkMessages(IProfileExpression config)
+        {
+            config.CreateMap<FromServer.JoinGameLobbyResult, JoinGameLobbyResult>();
+            config.CreateMap<FromServer.HandshakeResult, HandshakeResult>();
+            config.CreateMap<FromServer.IsAnyLobbyLookingForPlayersResult, LookingForPlayersResult>();
+        }
+
+        private static void MapDtos(IProfileExpression config)
+        {
+            config.CreateMap<PlayerDto, Player>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Color, opt => opt.Ignore())
+                .ForMember(dest => dest.IsLocalPlayer, opt => opt.Ignore())
+                .ForMember(dest => dest.Nr, opt => opt.Ignore())
+                .ReverseMap();
+
+            config.CreateMap<GameLobby.GameLobby, GameLobbyDto>().ReverseMap();
         }
     }
 }
--- src/Monoka/Server/Startup/AutoMapperBootstrapper.cs
+++ src/Monoka/Server/Startup/AutoMapperBootstrapper.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using Autofac;
 using AutoMapper;
+using Monoka.Common.Dto;
+using Monoka.Common.Network.Messages;
+using Monoka.Server.GameLobby;
+using Monoka.Server.GameSession;
+using Monoka.Server.Model;
 
 namespace Monoka.Server.Startup
 {
@@ -30,53 +35,53 @@ namespace Monoka.Server.Startup
 
         private static void Configure(IMapperConfigurationExpression config)
         {
-            //MapNetworkMessages(config);
-            //MapDtos(config);
+            MapNetworkMessages(config);
+            MapDtos(config);
         }
 
-        //private static void MapNetworkMessages(IMapperConfigurationExpression config)
-        //{
-        //    config.CreateMap<FromClient.JoinGameLobby, GameLobbyManager.JoinGameLobby>()
-        //        .ForMember(dest => dest.LobbyId, opt => opt.MapFrom(src => src.LobbyId))
-        //        .ForMember(dest => dest.PlayerId, opt => opt.MapFrom(src => src.PlayerId))
-        //        .ForMember(dest => dest.AsAdmin, opt => opt.Ignore());
+        private static void MapNetworkMessages(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<FromClient.JoinGameLobby, GameLobbyManager.JoinGameLobby>()
+                .ForMember(dest => dest.LobbyId, opt => opt.MapFrom(src => src.LobbyId))
+                .ForMember(dest => dest.PlayerId, opt => opt.MapFrom(src => src.PlayerId))
+                .ForMember(dest => dest.AsAdmin, opt => opt.Ignore());
 
-        //    config.CreateMap<GameLobbyManager.JoinGameLobbyResult, FromServer.JoinGameLobbyResult>();
-        //    config.CreateMap<FromClient.PlayerReady, GameLobbyManager.PlayerReady>();
-        //    config.CreateMap<FromClient.IsAnyLobbyLookingForPlayers, GameLobbyManager.IsAnyLobbyLookingForPlayers>();
-        //    config.CreateMap<FromClient.DonePreparing, GameSession.PlayerFinishedLoading>()
-        //        .ForSourceMember(src => src.GameSessionId, opt => opt.Ignore());
-        //}
+            config.CreateMap<GameLobbyManager.JoinGameLobbyResult, FromServer.JoinGameLobbyResult>();
+            config.CreateMap<FromClient.PlayerReady, GameLobbyManager.PlayerReady>();
+            config.CreateMap<FromClient.IsAnyLobbyLookingForPlayers, GameLobbyManager.IsAnyLobbyLookingForPlayers>();
+            config.CreateMap<FromClient.DonePreparing, GameSession.GameSession.PlayerFinishedLoading>()
+                .ForSourceMember(src => src.GameSessionId, opt => opt.Ignore());
+        }
 
-        //private static void MapDtos(IMapperConfiguration config)
-        //{
-        //    config.CreateMap<GameLobby, GameLobbyDto>()
-        //        .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-        //        .ForMember(dest => dest.Players, opt => opt.MapFrom(src => src.Players))
-        //        .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
-        //        .ReverseMap();
+        private static void MapDtos(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<GameLobby.GameLobby, GameLobbyDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Players, opt => opt.MapFrom(src => src.Players))
+                .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))
+                .ReverseMap();
 
-        //    config.CreateMap<PlayerDto, GameLobbyPlayer>().ReverseMap();
+            config.CreateMap<PlayerDto, GameLobbyPlayerDto>().ReverseMap();
 
-        //    config.CreateMap<PlayerDto, Player>()
-        //        .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-        //        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-        //        .ForSourceMember(src => src.IsReady, opt => opt.Ignore())
-        //        .ForSourceMember(src => src.IsAdmin, opt => opt.Ignore())
-        //        .ReverseMap();
+            config.CreateMap<PlayerDto, Player>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForSourceMember(src => src.IsReady, opt => opt.Ignore())
+                .ForSourceMember(src => src.IsAdmin, opt => opt.Ignore())
+                .ReverseMap();
 
-        //    config.CreateMap<GameLobbyPlayer, Player>()
-        //        .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-        //        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-        //        .ForSourceMember(src => src.IsAdmin, opt => opt.Ignore())
-        //        .ForSourceMember(src => src.IsReady, opt => opt.Ignore())
-        //        .ReverseMap();
+            config.CreateMap<GameLobbyPlayerDto, Player>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForSourceMember(src => src.IsAdmin, opt => opt.Ignore())
+                .ForSourceMember(src => src.IsReady, opt => opt.Ignore())
+                .ReverseMap();
 
-        //    config.CreateMap<GameSessionPlayerDto, Player>()
-        //        .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-        //        .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
-        //        .ForSourceMember(src => src.FinishedLoading, opt => opt.Ignore())
-        //        .ReverseMap();
-        //}
+            config.CreateMap<GameSessionPlayerDto, Player>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForSourceMember(src => src.FinishedLoading, opt => opt.Ignore())
+                .ReverseMap();
+        }
     }
 }
